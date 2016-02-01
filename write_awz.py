4d61fab87683f81e1790d28aced81e6461c37963
tal@whatexit.org <tal@whatexit.org>
2016-02-01 20:07:23 +0000

    Reorder books

--- write_awz.py
+++ write_awz.py
@@ -30,8 +30,8 @@ import creds
 
 PRODUCTS = [
 	'B0199EW96U', # Year In White: Kindle
-	'0813571200', # Year In White: Hardcover
 	'0813571197', # Year In White: Paperback
+	'0813571200', # Year In White: Hardcover
 	'1573980420', # April Fools RFCs: Paperback
 	'B00N7N2CRQ', # The Practice of Cloud: Kindle
 	'032194318X', # The Practice of Cloud: Paperback
@@ -40,8 +40,8 @@ PRODUCTS = [
 	'B0026OR2WM', # Time Mgmt for Sysadmins: Kindle
 	'0596007833', # Time Mgmt for Sysadmins: Paperback
 	'B00AZRBLHO', # Phoenix Project: Kindle
-	'0988262592', # Phoenix Project: Hardcover
 	'0988262509', # Phoenix Project: Paperback
+	'0988262592', # Phoenix Project: Hardcover
 	'B00VATFAMI', # Phoenix Project: Audible
 ]
 
